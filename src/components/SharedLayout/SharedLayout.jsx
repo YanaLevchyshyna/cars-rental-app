3b4ae8afa518c9eadd870effea5b2789fd548264
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-01-10 23:40:29 +0200

    changed styles in the SharedLayout Component

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,22 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import { Container, Header, Link } from './SharedLayout.styled';
+import {
+  Container,
+  Header,
+  HeaderContainer,
+  Link,
+} from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
-    <Container>
+    <>
       <Header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="catalog">Cars</Link>
-          <Link to="favorites">Favorite cars</Link>
-        </nav>
+        <Container>
+          <HeaderContainer>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="catalog">Cars</Link>
+              <Link to="favorites">Favorite cars</Link>
+            </nav>
+          </HeaderContainer>
+        </Container>
       </Header>
 
       <Suspense>
         <Outlet />
       </Suspense>
-    </Container>
+    </>
   );
 };
