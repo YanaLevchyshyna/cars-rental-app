9c17c7acbc22027edda6cea2df8d39663bc74172
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-01-12 23:46:53 +0200

    added the method configureStore.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,47 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import carsSlice from './carsSlice';
 import favoriteSlice from './favoriteSlice';
 
-export const store = configureStore({
-  reducer: {
-    cars: carsSlice,
-    favorite: favoriteSlice,
-  },
+// export const store = configureStore({
+//   reducer: {
+//     cars: carsSlice,
+//     favorite: favoriteSlice,
+//   },
+// });
+
+export const reducer = combineReducers({
+  cars: carsSlice,
+  favorite: favoriteSlice,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedContactsReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
