ff67aaaf2bc3468916e15aa784f70e3dc8f9e32a
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-03-05 15:21:52 +0200

    the Home component was changed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,22 @@ function Home() {
   return (
     <>
       <main>
-        <Container></Container>
+        <Container>
+          <div>
+            <h1
+              style={{
+                position: 'absolute',
+                top: '15%',
+                right: '5%',
+                color: '#0b0b0b',
+                fontFamily: 'Anta',
+              }}
+            >
+              Faster, easier, stylish - feel the freedom <br /> with our Car
+              Rental service!
+            </h1>
+          </div>
+        </Container>
       </main>
     </>
   );
